a54cd1fc45894a37b8365643e004f5b6c434efd7
Abbas.H <accessemail111@gmail.com>
2023-01-18 16:01:15 +0330

    Add files via upload

---
+++ PejwakBaran/PejwakBaran/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace PejwakBaran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runProgram = true;
+            while (runProgram)
+            {
+                Console.WriteLine();
+                Console.WriteLine("*******************Menu***********************");
+                Console.WriteLine("Please choose a number:");
+                Console.WriteLine("1- Validate a string");
+                Console.WriteLine("2- Exit");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+
+                        string input;
+                        // Get string from user
+                        Console.WriteLine("Please enter your string:");
+                        input = Console.ReadLine();
+
+                        // Check if input is empty
+                        if (String.IsNullOrWhiteSpace(input))
+                        {
+                            Console.WriteLine("Input string is empty or whitespace");
+                        }
+                        else
+                        {
+                            // Check string
+                            bool result = Solution.IsValid(input);
+
+                            if (result)
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Input string is valid.");
+                            }
+                            else
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Input string is not valid.");
+                            }
+                        }
+
+                        break;
+
+                    case "2":
+                        runProgram = false;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+                
+        }
+    }
+}
---
+++ PejwakBaran/PejwakBaran/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace PejwakBaran
+{
+    class Solution
+    {
+
+        /// <summary>
+        /// This method gets an input string and check if its brackets are valid or not
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool IsValid( string input)
+        {
+            Stack stack = new Stack();
+
+            foreach (char currentChar in input.ToCharArray())
+            {
+                if (currentChar == '(' || currentChar == '{' || currentChar == '[')
+                {
+                    // Push to stack if it is opening bracket
+                    stack.Push(currentChar); 
+                }
+
+                // Compare current character with top of stack
+                else if ( stack.Count != 0 && currentChar == ')' && (char)stack.Peek() == '(')
+                {
+                    stack.Pop();
+                }
+                else if ( stack.Count != 0 && currentChar == '}' && (char)stack.Peek() == '{')
+                {
+                    stack.Pop();
+                }
+                else if ( stack.Count != 0 && currentChar == ']' && (char)stack.Peek() == '[')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    // There is no match character on top of the stack
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
